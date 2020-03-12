b8bdd7d9034f841026f96c1acd020ee2686a4dac
rahman <rahman@localhost>
2020-03-12 16:54:33 +0700

    Update BaseClass

--- src/test/java/sampleproject/BaseClass.java
+++ src/test/java/sampleproject/BaseClass.java
@@ -27,35 +27,28 @@ public class BaseClass {
 		try {
 
 			DesiredCapabilities caps = new DesiredCapabilities();
-			
+
 			// Simulator
-						caps.setCapability("platformName", "iOS");
-						caps.setCapability("platformVersion", "13.3");
-						caps.setCapability("deviceName", "iPhone 11 Pro Max");
-						caps.setCapability("app", "/Users/rahman/Desktop/UICatalog.app");
-						caps.setCapability("noReset", true);
-			
+			caps.setCapability("platformName", "iOS");
+			caps.setCapability("platformVersion", "13.3");
+			caps.setCapability("deviceName", "iPhone 11 Pro Max");
+			caps.setCapability("app", "/Users/rahman/Desktop/UICatalog.app");
+			caps.setCapability("noReset", true);
+
 			// Real Device iPhone 5
 //			caps.setCapability("platformName", "iOS");
 //			caps.setCapability("platformVersion", "10.2");
 //			caps.setCapability("deviceName", "MTMD13");
 //			caps.setCapability("udid", "9a5e8c7c8a10e3c171396d1d6d999a4b23593a12");
 //			caps.setCapability("app", "/Users/rahman/Desktop/UICatalog.app");
-			
+
 			caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
 
-
 			URL url = new URL("http://127.0.0.1:4723/wd/hub");
-			
+
 //			driver = new AppiumDriver<MobileElement>(url, caps);
 			driver = new IOSDriver<IOSElement>(url, caps);
-			
-//			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//			caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "30");
-			
-			
-		
-			
+
 		} catch (Exception exp) {
 			System.out.println("Cause is : " + exp.getCause());
 			System.out.println("Message is : " + exp.getMessage());
@@ -63,23 +56,16 @@ public class BaseClass {
 		}
 
 	}
-	
+
 //	@Test
 //	public void sampleTest() throws InterruptedException {
 //		System.out.println("Ï am inside sample Test");
 //		Thread.sleep(10000);
 //	}
-	
-	
-	
-//	private void Sleep(int i) {
-//		// TODO Auto-generated method stub
-//		
-//	}
 
 	@AfterTest
 	public void teardown() {
-		//close the app
+		// close the app
 		driver.quit();
 	}
 //	public void uninstallApp() throws InterruptedException {
